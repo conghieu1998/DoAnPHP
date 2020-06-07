e2b5441cba05fe9b3c949afbb8d50a93b75d8179
nguyenhoangtan <tannguyen20082016@gmail.com>
2020-06-08 00:23:29 +0700

    Footer and oder

---
+++ application/views/site/footer.php
@@ -0,0 +1,61 @@
+<div class="row" >
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" id="bank">
+				<div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 no-padding">
+				    <div class="footer-info">            
+				      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+				        <a title=""><strong>VỀ CHÚNG TÔI</strong></a>
+				          <li> <a href="#" title="">Giới thiệu về shop</a></li> 
+				          <li> <a href="#" title="">Các mức chế tài vi phạm</a></li> 
+				          <li> <a href="#" title="">Quy chế hoạt động</a></li>
+				          <li> <a href="#" title="">Sản phẩm yêu thích</a></li> 
+				          <li> <a href="#" title="">Bảo mật thông tin</a></li>  
+				          <li> <a href="#" title="">Mua sỉ và hợp tác</a></li> 
+				      </div>
+				      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+				        <a title=""><strong>DỊCH VỤ KHÁCH HÀNG</strong></a>
+				          <li> <a href="#" title="">Hướng dẫn mua hàng</a></li>
+				          <li> <a href="#" title="">Vận chuyển và giao hàng</a></li>
+				          <li> <a href="#" title="">Bảo hành và đổi trả</a></li>
+				          <li> <a href="#" title="">Xử lý khiếu nại</a></li>
+				          <li> <a href="#" title="">Phàn nàn và góp ý</a></li>  
+				          <li> <a href="#" title="">Yêu cầu mẫu mới</a></li> 
+				           
+				      </div>
+				      <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+				        <a title=""><strong>LINK TÌM KIẾM</strong></a>        
+				        <li> <a href="http://localhost/sneakerz/khuyen-mai" title="">Xả kho</a></li>        
+				          <li> <a href="http://localhost/sneakerz/adidas-c10" title="">Giày Adidas</a></li> 
+				          <li> <a href="http://localhost/sneakerz/nike-c11" title="">Giày Nike</a></li>
+				          <li> <a href="http://localhost/sneakerz/new-balance-c12" title="">Giày New Balance</a>
+				          </li> 
+				          <li> <a href="http://localhost/sneakerz/vans-c13" title="">Giày Vans</a></li>
+			        	  <li> <a href="http://localhost/sneakerz/puma-c14" title="">Giày Puma</a></li>    
+				      </div>
+				    </div>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+					<span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>Miễn phí giao hàng toàn quốc cho đơn hàng trên 1.000.000đ.<br>
+				    <span class="glyphicon glyphicon-credit-card" aria-hidden="true"></span>Thanh toán bằng tiền mặt sau khi nhận và kiểm tra chất lượng.<br>
+				    <span class="glyphicon glyphicon-refresh" aria-hidden="true"></span>Dễ dàng đổi nếu size không vừa hoặc sản phẩm không đúng.<br>
+				    <span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>Không phải chờ order. Mẫu mới update liên tục mỗi tháng.<br>
+				  </div>
+			</div>
+		</div>
+		<div class="row" id="footer">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+					<address>
+					  <strong>Cửa hàng Sneakerz và thời trang đường phố hàng đầu tại VN.</strong><br>
+					  <span class="glyphicon glyphicon-home" aria-hidden="true"></span> Địa chỉ: 48B Thạch Thị Thanh, Tân Định, Q1, HCM<br>
+					  <span class="glyphicon glyphicon-phone" aria-hidden="true"></span> Điện thoại: 0903 150 443<br>
+					  <span class="glyphicon glyphicon-time" aria-hidden="true"></span> Mở cửa: Thứ 2-7: 10h-21h (CN: 13h-20h)<br>
+					</address>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 text-right">
+					<a href="#"><img src="<?php echo base_url(); ?>upload/icon/facebook.png" alt=""></a>
+					<a href="#"><img src="<?php echo base_url(); ?>upload/icon/twitter.png" alt=""></a>
+					<a href="#"><img src="<?php echo base_url(); ?>upload/icon/google.png" alt=""></a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
